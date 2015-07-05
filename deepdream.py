a266df10fd6952450c4cc7f15c0b7d8dde0d97a6
Gene Kogan <kogan.gene@gmail.com>
2015-07-05 18:20:45 -0400

    playing with parametric animation

--- deepdream.py
+++ deepdream.py
@@ -1,4 +1,6 @@
 from cStringIO import StringIO
+import math
+import random
 import numpy as np
 import scipy.ndimage as nd
 import PIL.Image
@@ -9,6 +11,7 @@ import caffe
 
 pathToOutput = '/Users/gene/Desktop/output/'
 pathToCaffe = '/Users/gene/Code/Python/caffe/'
+
 
 def setOutputDirectory(pathToOutput_):
 	pathToOutput = pathToOutput_;
@@ -21,6 +24,12 @@ def showarray(a, fmt='jpeg'):
 	f = StringIO()
 	PIL.Image.fromarray(a).save(f, fmt)
 	display(Image(data=f.getvalue()))
+
+def preprocess(net, img):
+	return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
+
+def deprocess(net, img):
+	return np.dstack((img + net.transformer.mean['data'])[::-1])
 
 def setupModel(model_):
 	model_path = pathToCaffe+'models/bvlc_googlenet/'
@@ -43,14 +52,6 @@ def setupModel(model_):
 	net = caffe.Classifier('tmp.prototxt', param_fn, mean = np.float32([104.0, 116.0, 122.0]), channel_swap = (2,1,0))
 	return net
 
-# a couple of utility functions for converting to and from Caffe's input image layout
-def preprocess(net, img):
-	return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
-
-def deprocess(net, img):
-	return np.dstack((img + net.transformer.mean['data'])[::-1])
-
-# compute a step
 def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=True):
 	'''Basic gradient ascent step.'''
 	src = net.blobs['data'] # input image is stored in Net's 'data' blob	
@@ -68,7 +69,9 @@ def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=Tru
 		bias = net.transformer.mean['data']
 		src.data[:] = np.clip(src.data, -bias, 255-bias)
 
-def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, end='inception_4c/output', clip=True, **step_params):
+#inception_3b/5x5_reduce
+#inception_4c/output
+def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, end='inception_3b/5x5_reduce', jitter=32, clip=True, **step_params):
 	# prepare base images for all octaves
 	octaves = [preprocess(net, base_img)]
 	for i in xrange(octave_n-1):
@@ -77,21 +80,13 @@ def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, end='incep
 	detail = np.zeros_like(octaves[-1]) # allocate image for network-produced details
 	for octave, octave_base in enumerate(octaves[::-1]):
 		h, w = octave_base.shape[-2:]
-		if octave > 0:
-			# upscale details from the previous octave
+		if octave > 0:	# upscale details from the previous octave
 			h1, w1 = detail.shape[-2:]
 			detail = nd.zoom(detail, (1, 1.0*h/h1,1.0*w/w1), order=1)
 		src.reshape(1,3,h,w) # resize the network's input image size
 		src.data[0] = octave_base+detail
 		for i in xrange(iter_n):
-			make_step(net, end=end, clip=clip, **step_params)
-			# visualization
-			vis = deprocess(net, src.data[0])
-			if not clip: # adjust image contrast if clipping is disabled
-				vis = vis*(255.0/np.percentile(vis, 99.98))
-			showarray(vis)
-			print octave, i, end, vis.shape
-			clear_output(wait=True)
+			make_step(net, end=end, clip=clip, jitter=jitter, **step_params)
 		# extract details produced on the current octave
 		detail = src.data[0]-octave_base
 	#returning the resulting image
@@ -101,16 +96,11 @@ def loadImage(pathToImage):
 	img = np.float32(PIL.Image.open(pathToImage))
 	return img
 
-def makeDeepDream(img, classifier, name, generations, iterations, octaves, octave_scale, scaleZoom):
-	frame = img
-	for gen in range(0, generations):
-		print "gen_"+str(gen)
-		newImagePath = pathToOutput +'/' + name + "_" + str(iterations) + "_" + str(octaves) + "_" + str(octave_scale)
-		if (generations > 1):
-			newImagePath += '('+str(gen)+')'
-		newImagePath += '.jpg'
-		frame = deepdream(classifier, frame, iterations, octaves, octave_scale)
-		PIL.Image.fromarray(np.uint8(np.clip(frame, 0, 255))).save(newImagePath, 'jpeg')
-		h, w = frame.shape[:2]
-		frame = nd.affine_transform(frame, [1-scaleZoom,1-scaleZoom,1], [h*scaleZoom/2,w*scaleZoom/2,0], order=1)
-	
+def makeDeepDream(img, classifier, end, name, iterations, octaves, octave_scale, jitter, scaleZoom):
+	newImagePath = pathToOutput+'/'+name+"_i"+str(iterations)+"_o"+str(octaves)+"_os"+str(octave_scale)+"_j"+str(jitter)+'.png'
+	frame = deepdream(classifier, img, iterations, octaves, octave_scale, end, jitter)
+	PIL.Image.fromarray(np.uint8(np.clip(frame, 0, 255))).save(newImagePath, 'png')
+	h, w = frame.shape[:2]
+	frame = nd.affine_transform(frame, [1-scaleZoom,1-scaleZoom,1], [h*scaleZoom/2,w*scaleZoom/2,0], order=1)
+	return frame
+		
--- main.py
+++ main.py
@@ -1,18 +1,45 @@
+"""
+export PYTHONPATH=/Users/gene/Code/Python/caffe/python
+export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/cuda/lib:$HOME/anaconda/lib:/usr/local/lib:/usr/lib:/opt/intel/composer_xe_2015.3.187/compiler/lib:/opt/intel/composer_xe_2015.3.187/mkl/lib
+"""
+
 import deepdream as dd
+import math
 
+def animation():
+	imagePath = '/Users/gene/Documents/Processing/noiseField/frames/noiseframeFull2.jpg'
+	#imagePath = '/Users/gene/Documents/Processing/noiseField/frames/noiseframeFull2Small.jpeg'
+	classifier_index = 1
+	name = 'testSeq1Large/test'
+	end1 = 'inception_4c/output'
+	end2 = 'inception_3b/5x5_reduce'
+	generations = 720
+	iterations = 10
+	octaves = 4
+	octave_scale = 1.4
+	jitter = 16
+	scaleZoom = 0.05
+	
+	img = dd.loadImage(imagePath)
+	classifier = dd.setupModel(classifier_index)
+	frame = img
+	
+	for gen in range(generations):
+		iterations = int(7 + 6*math.sin(gen*0.1))
+		octaves = int(5+4*math.sin(gen*0.2+5))
+		octave_scale = round(1.2 + 0.2*math.sin(gen*0.3-7), 1)
+		scaleZoom = round(0.03 + 0.02*math.sin(gen*0.02+19), 2)
+		
+		if int(gen / 44) % 2 == 0:
+			end = end2
+		else:
+			end = end1
+	
+		print "G "+str(gen)+"/"+str(generations)+" ("+end+") iter "+str(iterations)+", octaves "+str(octaves)+", oscale "+str(octave_scale)+", jitter "+str(jitter)+", scaleZoom "+str(scaleZoom)
+		frame = dd.makeDeepDream(frame, classifier, end, name+'%04d'%gen, iterations, octaves, octave_scale, jitter, scaleZoom)	
 
-myDirectory = '/Users/gene/Code/Python/deepdream/pictures/'
 
 dd.setPathToCaffe('/Users/gene/Code/Python/caffe/')
 dd.setOutputDirectory('/Users/gene/Desktop/output/')
 
-classifier = dd.setupModel(1)
-img = dd.loadImage(myDirectory+'alec1.jpg')
-prefix = 'alec/alec'
-
-generations = 1
-for iterations in range(5,28,7):
-	for octaves in range(1,11,3):
-		for octave_scale in [0.7, 2.1]:
-			print "generate " + str(iterations) + ", " + str(octaves) + ", " + str(octave_scale)
-			dd.makeDeepDream(img, classifier, prefix, generations, iterations, octaves, octave_scale, 0.05)
+animation()
